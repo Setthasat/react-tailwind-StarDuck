bea3dfad4b6d6cc8c8650a6ac3550eff6e67ed9e
safe <setthasat2007@gmail.com>
2023-01-27 13:42:48 +0700

    responsive fixed

--- src/components/TopsellSingleCard.jsx
+++ src/components/TopsellSingleCard.jsx
@@ -8,10 +8,11 @@ function TopsellSingleCard({ item }) {
             <div className='bg-gradient-to-tr from-yellow-500 to-yellow-200 m-3 h-[70px] rounded-b-lg rounded-r-lg grid grid-cols-2'>
                 <img src={Duck} width='90px' height='50px' className='rounded-lg m-3 shadow-lg' />
                 <div>
-                    <p className='text-white flex justify-center items-center mt-1 mb-1 font-sans text-[17px]  md:text-[20px]'>
+                    <p className='text-white flex flex-col items-center mt-1 mb-1 font-sans text-[17px]  md:text-[20px]'>
                         {item.name}
+                        <motion.button whileHover={{ scale: 1.2 }} className='md:mt-[1px]  text-[17px] md:text-[19px] border border-white rounded-xl text-white'><span className='m-2'>Buy</span></motion.button>
                     </p>
-                    <motion.button whileHover={{ scale: 1.2 }} className='ml-5 md:ml-[10.6rem] text-[17px] md:text-[19px] md:-mt-4 border border-white rounded-xl text-white'><span className='m-2'>Buy</span></motion.button>
+                    
                 </div>
             </div>
         </div>
