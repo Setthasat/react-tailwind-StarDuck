1618fcc18a8a2b2a76d291bba56d486c8e456e33
safe <setthasat2007@gmail.com>
2023-01-28 21:15:28 +0700

    responsive fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,19 +6,19 @@ function Hero() {
   const darkmode = useStore((state => state.darkmode))
   return (
     <div>
-      <div className='max-w-[1640px] mx-auto '>
+      <div className='w-full mx-auto '>
         <div className='relative max-h-[500px]'>
         <div className='max-h-[500px] relative'>
           <div className={darkmode ? 'absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center' : 'absolute w-full h-full text-gray-200 max-h-[500px] bg-black/50 flex flex-col justify-center'}>
-            <div className='mb-24'>
+            <div className='absolute left-0 top-0'>
               <Navbar />
             </div>
-            <div className='mb-1'>
+            <div className='mt-60'>
               <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-300'>Welcome To</h1>
               <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-300'>Star<span className='text-yellow-500'>Duck</span> Store</h1>
             </div>
           </div>
-          <img className='w-full max-h-[500px] object-cover border shadow-2xl' src="https://tenrandomfacts.com/wp-content/uploads/2016/08/Rubber-Duck.jpg" alt="/" />
+          <img className='w-full h-[500px] object-cover border shadow-2xl' src="https://tenrandomfacts.com/wp-content/uploads/2016/08/Rubber-Duck.jpg" alt="/" />
         </div>
         </div>
         {/* <div className='relative flex flex-col bg-gradient-to-r from-bg-[#838FA5] to'>
